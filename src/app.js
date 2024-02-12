4134a5f0b35edcf2dac2d1dee803234c837a969c
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-12 11:40:54 +0000

    added json middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import readerRoutes from './routes/reader.js';
-
 export const app = express();
+
+app.use(express.json());
 app.use('/readers', readerRoutes);
