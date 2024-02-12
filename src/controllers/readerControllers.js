5a5f2437f7c8904f78c55b8383a9ad61036e4124
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-12 11:44:15 +0000

    updated readerController to use the req body to create a new record

--- src/controllers/readerControllers.js
+++ src/controllers/readerControllers.js
@@ -1,3 +1,14 @@
-export const createReader = (req, res) => {
+import setupDatabase from '../models/index.js';
+const db = await setupDatabase();
+const Reader = db.Reader;
+
+export const createReader = async (req, res) => {
+  const reader = await Reader.create({
+    name: `${req.body.name}`,
+    email: `${req.body.email}`,
+  });
+  req.body.id = reader.id;
+  res.json(req.body);
+
   res.sendStatus(200);
 };
