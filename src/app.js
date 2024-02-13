cf42ce975135070b4f9baffa5546cca0a2b99d21
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-13 16:39:45 +0000

    after manually setting the content-types on my test requests, i realised body parser would be simpler

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import 'dotenv/config';
 import readerRoutes from './routes/reader.js';
+import bodyParser from 'body-parser';
 export const app = express();
 
-app.use(express.json());
+app.use(bodyParser.json());
 app.use('/readers', readerRoutes);
