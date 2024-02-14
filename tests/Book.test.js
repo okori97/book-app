e95ecb0d23757ed1cffa61dafc190bda978a9383
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-14 18:31:29 +0000

    ammend book test nesting

--- tests/Book.test.js
+++ tests/Book.test.js
@@ -41,148 +41,148 @@ describe('/Books', () => {
         expect(response.body.error).to.eql('Bad request');
       });
     });
+  });
 
-    describe('with records in the database', () => {
-      let books;
-      beforeEach(async () => {
-        books = await Book.bulkCreate([
-          {
-            title: 'The Trial',
-            author: 'Franz Kafka',
-            genre: 'Surreal',
-            ISBN: 'GB29 NWBK 6016 1331 9268 19',
-          },
-          {
-            title: 'Infinite Jest',
-            author: 'David F. Wallace',
-            genre: 'Postmodern',
-            ISBN: 'GB33 NWBK 4024 1551 9227 86',
-          },
-        ]);
+  describe('with records in the database', () => {
+    let books;
+    beforeEach(async () => {
+      books = await Book.bulkCreate([
+        {
+          title: 'The Trial',
+          author: 'Franz Kafka',
+          genre: 'Surreal',
+          ISBN: 'GB29 NWBK 6016 1331 9268 19',
+        },
+        {
+          title: 'Infinite Jest',
+          author: 'David F. Wallace',
+          genre: 'Postmodern',
+          ISBN: 'GB33 NWBK 4024 1551 9227 86',
+        },
+      ]);
 
-        books = books.map((record) => record.get({ plain: true }));
-      });
+      books = books.map((record) => record.get({ plain: true }));
+    });
 
-      describe('GET /books', () => {
-        it('gets all the records in the database', async () => {
-          const response = await request(app).get('/books');
+    describe('GET /books', () => {
+      it('gets all the records in the database', async () => {
+        const response = await request(app).get('/books');
 
-          expect(response.status).to.equal(200);
-          expect(response.body.length).to.equal(2);
+        expect(response.status).to.equal(200);
+        expect(response.body.length).to.equal(2);
 
-          response.body.forEach((record) => {
-            const expected = books.find((book) => {
-              return book.id == record.id;
-            });
-
-            expect(record.title).eql(expected.title);
-            expect(record.author).eql(expected.author);
+        response.body.forEach((record) => {
+          const expected = books.find((book) => {
+            return book.id == record.id;
           });
-        });
 
-        it('returns a 404 if there are no books in the database', async () => {
-          Book.destroy({ where: {} });
-          const response = await request(app).get('/books');
-
-          expect(response.status).to.equal(404);
-          expect(response.body.error).to.equal('No books found!');
+          expect(record.title).eql(expected.title);
+          expect(record.author).eql(expected.author);
         });
       });
 
-      describe('GET /books/:id', () => {
-        it('gets a single book by id', async () => {
-          const response = await request(app).get(`/books/${books[0].id}`);
-          const expected = books[0];
+      it('returns a 404 if there are no books in the database', async () => {
+        Book.destroy({ where: {} });
+        const response = await request(app).get('/books');
 
-          expect(response.status).to.equal(200);
-          expect(response.body.title).to.equal(expected.title);
-          expect(response.body.author).to.equal(expected.author);
-        });
+        expect(response.status).to.equal(404);
+        expect(response.body.error).to.equal('No books found!');
+      });
+    });
 
-        it('returns a 404 if no book does not exist', async () => {
-          const response = await request(app).get('/books/1234');
+    describe('GET /books/:id', () => {
+      it('gets a single book by id', async () => {
+        const response = await request(app).get(`/books/${books[0].id}`);
+        const expected = books[0];
 
-          expect(response.status).to.equal(404);
-          expect(response.body.error).to.eql('Book not found');
-        });
+        expect(response.status).to.equal(200);
+        expect(response.body.title).to.equal(expected.title);
+        expect(response.body.author).to.equal(expected.author);
       });
 
-      describe('PATCH /books/:id', () => {
-        it('updates a books title in the database', async () => {
-          const response = await request(app)
-            .patch(`/books/${books[1].id}`)
-            .send({
-              title: 'Broom of the System',
-            });
+      it('returns a 404 if no book does not exist', async () => {
+        const response = await request(app).get('/books/1234');
 
-          const existingBook = books[1];
-          const updatedBook = await Book.findByPk(books[1].id);
+        expect(response.status).to.equal(404);
+        expect(response.body.error).to.eql('Book not found');
+      });
+    });
 
-          expect(response.status).to.equal(200);
-          expect(updatedBook.title).to.not.eql(existingBook.title);
-          expect(updatedBook.author).to.eql(existingBook.author);
-          expect(updatedBook.ISBN).to.eql(existingBook.ISBN);
-        });
-
-        it('returns a 404 if book does not exist', async () => {
-          const response = await request(app).patch('/books/12344').send({
+    describe('PATCH /books/:id', () => {
+      it('updates a books title in the database', async () => {
+        const response = await request(app)
+          .patch(`/books/${books[1].id}`)
+          .send({
             title: 'Broom of the System',
           });
 
-          expect(response.status).to.equal(404);
-          expect(response.body.error).to.equal('Book not found!');
-        });
+        const existingBook = books[1];
+        const updatedBook = await Book.findByPk(books[1].id);
 
-        it('returns a 400 if request is not of the correct type', async () => {
-          const response = await request(app)
-            .patch(`/books/${books[1].id}`)
-            .send('badthing')
-            .set('Content-Type', 'text/html');
-
-          expect(response.status).to.equal(400);
-          expect(response.body.error).to.eql('Bad request');
-        });
+        expect(response.status).to.equal(200);
+        expect(updatedBook.title).to.not.eql(existingBook.title);
+        expect(updatedBook.author).to.eql(existingBook.author);
+        expect(updatedBook.ISBN).to.eql(existingBook.ISBN);
       });
 
-      describe('DELETE /books/:id', () => {
-        it('deletes a single book from the database', async () => {
-          const existingRecord = books[0];
-          const response = await request(app).delete(`/books/${books[0].id}`);
-          const deletedBook = await Book.findByPk(books[0].id);
-
-          const currentBooks = await Book.findAll({
-            raw: true,
-            attributes: ['title', 'author', 'genre', 'ISBN'],
-          });
-
-          expect(currentBooks.length).to.equal(books.length - 1);
-          expect(response.status).to.equal(200);
-          expect(deletedBook).to.equal(null);
-          expect(existingRecord).to.not.equal(null);
-          expect(response.body.success).eql('Book deleted');
+      it('returns a 404 if book does not exist', async () => {
+        const response = await request(app).patch('/books/12344').send({
+          title: 'Broom of the System',
         });
 
-        it('returns a 404 if book does not exist', async () => {
-          const response = await request(app).delete('/books/1234');
-          const currentBooks = await Book.findAll({
-            raw: true,
-            attributes: ['title', 'author', 'genre', 'ISBN'],
-          });
+        expect(response.status).to.equal(404);
+        expect(response.body.error).to.equal('Book not found!');
+      });
 
-          expect(currentBooks.length).to.equal(books.length);
-          expect(response.status).to.eql(404);
-          expect(response.body.error).to.eql('Book not found');
+      it('returns a 400 if request is not of the correct type', async () => {
+        const response = await request(app)
+          .patch(`/books/${books[1].id}`)
+          .send('badthing')
+          .set('Content-Type', 'text/html');
+
+        expect(response.status).to.equal(400);
+        expect(response.body.error).to.eql('Bad request');
+      });
+    });
+
+    describe('DELETE /books/:id', () => {
+      it('deletes a single book from the database', async () => {
+        const existingRecord = books[0];
+        const response = await request(app).delete(`/books/${books[0].id}`);
+        const deletedBook = await Book.findByPk(books[0].id);
+
+        const currentBooks = await Book.findAll({
+          raw: true,
+          attributes: ['title', 'author', 'genre', 'ISBN'],
         });
 
-        it('returns a 400 if request is not of the correct type', async () => {
-          const response = await request(app)
-            .delete(`/books/${books[0].id}`)
-            .send('badthing')
-            .set('Content-Type', 'text/html');
+        expect(currentBooks.length).to.equal(books.length - 1);
+        expect(response.status).to.equal(200);
+        expect(deletedBook).to.equal(null);
+        expect(existingRecord).to.not.equal(null);
+        expect(response.body.success).eql('Book deleted');
+      });
 
-          expect(response.status).to.eql(400);
-          expect(response.body.error).to.eql('Bad request');
+      it('returns a 404 if book does not exist', async () => {
+        const response = await request(app).delete('/books/1234');
+        const currentBooks = await Book.findAll({
+          raw: true,
+          attributes: ['title', 'author', 'genre', 'ISBN'],
         });
+
+        expect(currentBooks.length).to.equal(books.length);
+        expect(response.status).to.eql(404);
+        expect(response.body.error).to.eql('Book not found');
+      });
+
+      it('returns a 400 if request is not of the correct type', async () => {
+        const response = await request(app)
+          .delete(`/books/${books[0].id}`)
+          .send('badthing')
+          .set('Content-Type', 'text/html');
+
+        expect(response.status).to.eql(400);
+        expect(response.body.error).to.eql('Bad request');
       });
     });
   });
