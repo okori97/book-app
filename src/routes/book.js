74e5786adbcc6f5bde795e9dd9d3c7cb68703095
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-14 18:28:03 +0000

    add all book CRUD controllers

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import { findAll, createBook } from '../controllers/bookControllers.js';
+import {
+  findAll,
+  createBook,
+  findBook,
+  updateBook,
+  deleteBook,
+} from '../controllers/bookControllers.js';
 const bookRouter = express.Router();
 
 bookRouter.get('/', findAll);
+bookRouter.get('/:id', findBook);
+bookRouter.patch('/:id', updateBook);
+bookRouter.delete('/:id', deleteBook);
 bookRouter.post('/', createBook);
 
 export default bookRouter;
