c344e4b87b3c6d7c8661b33eee7b1f0a0caf0d09
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-15 18:24:02 +0000

    update reader model with validation and constraints

--- src/models/Reader.js
+++ src/models/Reader.js
@@ -1,8 +1,44 @@
 const ReaderModel = (connection, DataTypes) => {
   const schema = {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          true: true,
+          msg: 'Please input a name',
+        },
+        notEmpty: {
+          true: true,
+          msg: 'Please input a name',
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+        notNull: {
+          args: true,
+          msg: 'Please input a email',
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: true,
+          msg: 'Please add a password',
+        },
+        len: {
+          args: [9],
+          msg: 'Password must be more than 8 characters',
+        },
+      },
+    },
   };
 
   const ReaderModel = connection.define('Reader', schema);
