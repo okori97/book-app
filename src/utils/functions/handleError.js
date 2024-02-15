c633c7b63d761532d73c1485320643bad2c233cd
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-15 18:24:58 +0000

    update handleError to handle validation errors

--- src/utils/functions/handleError.js
+++ src/utils/functions/handleError.js
@@ -1,6 +1,10 @@
 const handleError = (error, res) => {
-  console.error(error);
-  return res.status(500).json({ error: error });
+  const validationError =
+    error.errors[0].message || error.errors[0].ValidationErrorItem.message;
+
+  validationError
+    ? res.status(400).json({ error: `${validationError}` })
+    : res.status(500).json({ error: error });
 };
 
 export default handleError;
