221bef0806d11863ce317e22c441f9989151f820
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-19 20:40:45 +0000

    feat: add new routes to app

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ import express from 'express';
 import 'dotenv/config';
 import readerRoutes from './routes/reader.js';
 import bookRoutes from './routes/book.js';
+import genreRoutes from './routes/genre.js';
+import authorRoutes from './routes/author.js';
 import bodyParser from 'body-parser';
 export const app = express();
 
 app.use(bodyParser.json());
 app.use('/readers', readerRoutes);
 app.use('/books', bookRoutes);
+app.use('/genres', genreRoutes);
+app.use('/authors', authorRoutes);
