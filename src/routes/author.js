48e99f80ce7ba4ee879e10aca2d53d4a891ff10c
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-19 22:50:41 +0000

    feat: add delete controller to author route

--- src/routes/author.js
+++ src/routes/author.js
@@ -4,11 +4,16 @@ import {
   findAuthor,
   findAll,
   updateAuthor,
+  deleteAuthor,
 } from '../controllers/authorControllers.js';
 
 const authorRouter = express.Router();
 
 authorRouter.route('/').post(createAuthor).get(findAll);
-authorRouter.route('/:id').get(findAuthor).patch(updateAuthor);
+authorRouter
+  .route('/:id')
+  .get(findAuthor)
+  .patch(updateAuthor)
+  .delete(deleteAuthor);
 
 export default authorRouter;
