f1ceb78601aece541e3ff215fd69772640422a06
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-19 21:30:38 +0000

    refactor : begin refactor of CRUD helpers, all POST tests passing

--- src/utils/functions/queries.js
+++ src/utils/functions/queries.js
@@ -1,152 +1,74 @@
+import { Book, Author, Reader, Genre } from '../../models/index.js';
+
+const getModel = (model) => {
+  const models = {
+    book: Book,
+    reader: Reader,
+    genre: Genre,
+    author: Author,
+  };
+
+  return models[model];
+};
+
 const createItem = async (model, req, res) => {
-  switch (model.name) {
-    case 'Book': {
-      let { title, author, genre, ISBN } = req.body || null;
-      const newBook = await model.create({
-        title: title,
-        author: author,
-        genre: genre,
-        ISBN: ISBN,
-      });
-
-      res.status(201).json(newBook);
-
-      break;
-    }
-    case 'Reader': {
-      const { name, email, password } = req.body || null;
-
-      const reader = await model.create({
-        name: name,
-        email: email,
-        password: password,
-      });
-      res.status(201).json(reader);
-      break;
-    }
-    case 'Genre': {
-      let { title, author, genre, ISBN } = req.body || null;
-      let Genre;
-      title == null || author == null
-        ? ''
-        : (Genre = await model.create({
-            genre: genre,
-          }));
-
-      break;
-    }
-    default:
-      break;
-  }
+  const Model = getModel(model);
+  console.log(req.body);
+  const item = await Model.create(req.body);
+  item ? res.status(201).json(item) : '';
 };
 
 const findItem = async (model, req, res) => {
-  switch (model.name) {
-    case 'Book': {
-      if (req.params.id == undefined) {
-        const books = await model.findAll({
-          raw: true,
-          attributes: ['title', 'author', 'genre', 'ISBN', 'id'],
-        });
-        books.length == 0
-          ? res.status(404).json({ error: 'No books found' })
-          : res.json(books);
-      } else {
-        const book = await model.findByPk(req.params.id);
-        book != null
-          ? res.json(book)
-          : res.status(404).json({ error: 'Book not found' });
-      }
-      break;
-    }
+  const Model = getModel(model);
 
-    case 'Reader': {
-      if (req.params.id == undefined) {
-        const readers = await model.findAll({
-          raw: true,
-          attributes: ['name', 'email', 'id'],
-        });
-        readers.length == 0
-          ? res.status(404).json({ error: 'No users found' })
-          : res.json(readers);
-      } else {
-        const reader = await model.findByPk(req.params.id, {
-          attributes: ['name', 'email', 'id'],
-        });
+  const item = await Model.findAll();
+  item.length != 0
+    ? res.json(item)
+    : res.status(404).json({ error: `No ${model}s found` });
+};
 
-        reader != null
-          ? res.json(reader)
-          : res.status(404).json({ error: 'User not found' });
-      }
-      break;
-    }
+const findItemByID = async (model, req, res) => {
+  const Model = getModel(model);
 
-    default:
-      break;
-  }
+  const item = await Model.findByPk(req.params.id);
+  item == null
+    ? res.status(404).json({ error: 'No books found' })
+    : res.json(item);
 };
 
 const updateItem = async (model, req, res) => {
-  switch (model.name) {
-    case 'Book': {
-      const { id } = req.params;
-      const { title } = req.body;
+  const Model = getModel(model);
 
-      const updatedBook = await model.update(
-        { title: title },
-        { where: { id: id } }
-      );
-
-      updatedBook == false
-        ? res.status(404).json({ error: 'Book not found' })
-        : res.json({ success: 'User updated' });
-      break;
-    }
-    case 'Reader': {
-      const { id } = req.params;
-      const { name } = req.body;
-
-      const updatedReader = await model.update(
-        { name: name },
-        { where: { id: id } }
-      );
-
-      updatedReader == false
-        ? res.status(404).json({ error: 'User not found' })
-        : res.json({ success: 'User updated' });
-      break;
-    }
-    default:
-      break;
-  }
+  const item = await Model.update(req.body, { where: { id: req.params.id } });
+  item == false
+    ? res.status(404).json({ error: `${model} not found` })
+    : res.json({ success: `${model} updated` });
 };
 
 const deleteItem = async (model, req, res) => {
-  switch (model.name) {
-    case 'Book': {
-      const deleteBook = await model.destroy({
-        where: { id: `${req.params.id}` },
-      });
-
-      deleteBook != false
-        ? res.json({ success: 'Book deleted' })
-        : res.status(404).json({ error: 'Book not found' });
-      break;
-    }
-    case 'Reader': {
-      const deleteReader = await model.destroy({
-        where: { id: `${req.params.id}` },
-      });
-
-      deleteReader != false
-        ? res.json({ success: 'User deleted' })
-        : res.status(404).json({ error: 'User not found' });
-
-      break;
-    }
-    default:
-      break;
-  }
+  res.json({});
+  //   switch (model.name) {
+  //     case 'Book': {
+  //       const deleteBook = await model.destroy({
+  //         where: { id: `${req.params.id}` },
+  //       });
+  //       deleteBook != false
+  //         ? res.json({ success: 'Book deleted' })
+  //         : res.status(404).json({ error: 'Book not found' });
+  //       break;
+  //     }
+  //     case 'Reader': {
+  //       const deleteReader = await model.destroy({
+  //         where: { id: `${req.params.id}` },
+  //       });
+  //       deleteReader != false
+  //         ? res.json({ success: 'User deleted' })
+  //         : res.status(404).json({ error: 'User not found' });
+  //       break;
+  //     }
+  //     default:
+  //       break;
+  //   }
 };
 
-export { createItem, findItem, updateItem, deleteItem };
+export { createItem, findItem, findItemByID, updateItem, deleteItem };
