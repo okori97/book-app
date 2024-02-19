61d39a41a7b25163b3f43aef5d3ad3f2843c3f5f
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-19 20:39:26 +0000

    feat: create all genre controllers

--- src/controllers/genreControllers.js
+++ src/controllers/genreControllers.js
@@ -7,15 +7,61 @@ import {
 
 import {
   createItem,
-  deleteItem,
-  updateItem,
   findItem,
+  updateItem,
+  deleteItem,
 } from '../utils/functions/queries.js';
 
 export const createGenre = async (req, res, next) => {
   try {
     isContentTypeJson(req)
       ? await createItem(Genre, req, res)
+      : requestError(res);
+  } catch (error) {
+    modelError(error);
+  }
+
+  next();
+};
+
+export const findAll = async (req, res, next) => {
+  try {
+    isContentTypeJson(req)
+      ? await findItem(Genre, req, res)
+      : requestError(res);
+  } catch (error) {
+    modelError(error);
+  }
+
+  next();
+};
+
+export const findGenre = async (req, res, next) => {
+  try {
+    isContentTypeJson(req)
+      ? await findItem(Genre, req, res)
+      : requestError(res);
+  } catch (error) {
+    modelError(error);
+  }
+
+  next();
+};
+export const updateGenre = async (req, res, next) => {
+  try {
+    isContentTypeJson(req)
+      ? await updateItem(Genre, req, res)
+      : requestError(res);
+  } catch (error) {
+    modelError(error);
+  }
+
+  next();
+};
+export const deleteGenre = async (req, res, next) => {
+  try {
+    isContentTypeJson(req)
+      ? await deleteItem(Genre, req, res)
       : requestError(res);
   } catch (error) {
     modelError(error);
