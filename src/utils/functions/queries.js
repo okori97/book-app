3703672399e0fc012bbf44df766ddc66b65041a2
Okori Lewis-McCalla <okorishakir@gmail.com>
2024-02-26 01:15:29 +0000

    feat: queries dynamically include associations and refactor queries

--- src/utils/functions/queries.js
+++ src/utils/functions/queries.js
@@ -11,48 +11,68 @@ const getModel = (model) => {
   return models[model];
 };
 
+const includeAssociations = (model) => {
+  const associations = {
+    book: [Genre],
+    genre: [Book],
+  };
+
+  return associations[model] || [];
+};
+
 const createItem = async (model, req, res) => {
   const Model = getModel(model);
-  const item = await Model.create(req.body);
-  item ? res.status(201).json(item) : '';
+  return await Model.create(req.body).then((item) => {
+    res.status(201).json(item);
+  });
 };
 
 const findItem = async (model, req, res) => {
   const Model = getModel(model);
 
-  const item = await Model.findAll({
+  return await Model.findAll({
+    include: includeAssociations(model),
     attributes: { exclude: ['createdAt', 'updatedAt'] },
+  }).then((item) => {
+    item.length != 0
+      ? res.json(item)
+      : res.status(404).json({ error: `No ${model}s found` });
   });
-  item.length != 0
-    ? res.json(item)
-    : res.status(404).json({ error: `No ${model}s found` });
 };
 
 const findItemByID = async (model, req, res) => {
   const Model = getModel(model);
 
-  const item = await Model.findByPk(req.params.id);
-  item == null
-    ? res.status(404).json({ error: `${model} not found` })
-    : res.json(item);
+  return await Model.findByPk(req.params.id, {
+    include: includeAssociations(model),
+  }).then((item) => {
+    // console.log('findbyID: ', item);
+    item == null
+      ? res.status(404).json({ error: `${model} not found` })
+      : res.json(item);
+  });
 };
 
 const updateItem = async (model, req, res) => {
   const Model = getModel(model);
 
-  const item = await Model.update(req.body, { where: { id: req.params.id } });
-  item == false
-    ? res.status(404).json({ error: `${model} not found` })
-    : res.json({ success: `${model} updated` });
+  return await Model.update(req.body, {
+    where: { id: req.params.id },
+  }).then((item) => {
+    item[0] == false
+      ? res.status(404).json({ error: `${model} not found` })
+      : res.json({ success: `${model} updated` });
+  });
 };
 
 const deleteItem = async (model, req, res) => {
   const Model = getModel(model);
 
-  const item = await Model.destroy({ where: { id: req.params.id } });
-  item != false
-    ? res.json({ success: `${model} deleted` })
-    : res.status(404).json({ error: `${model} not found` });
+  return await Model.destroy({ where: { id: req.params.id } }).then((item) => {
+    item > 0
+      ? res.json({ success: `${model} deleted` })
+      : res.status(404).json({ error: `${model} not found` });
+  });
 };
 
 export { createItem, findItem, findItemByID, updateItem, deleteItem };
